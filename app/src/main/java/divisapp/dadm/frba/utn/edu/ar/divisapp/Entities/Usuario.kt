b9869523b4336c655ea89455fdfb7d5a4f8f215e
Ezequiel Escobar <ezequieloscarescobar2@gmail.com>
2018-10-31 23:23:16 -0300

    Clean

--- app/src/main/java/divisapp/dadm/frba/utn/edu/ar/divisapp/Entities/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package divisapp.dadm.frba.utn.edu.ar.divisapp.Entities
-
-class Usuario {
-    var id: Int? = null
-    var nombreDeUsuario: String? = null
-    var contrasenia: String? = null
-    var configuracion: Configuracion? = null
-
-}
