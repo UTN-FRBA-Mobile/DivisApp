2bf34f3028458d983c3bc77fd45d3528aea5353c
Devesa Agustín <devesagustserwi@est.frba.utn.edu.ar>
2018-11-26 00:13:50 -0300

    small changes.

--- app/src/main/java/com/gradientepolimorfico/monedapp/Fragments/AddSaldoDialogFragment.kt
+++ app/src/main/java/com/gradientepolimorfico/monedapp/Fragments/AddSaldoDialogFragment.kt
@@ -1,9 +1,11 @@
 package com.gradientepolimorfico.monedapp.Fragments
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -21,21 +23,16 @@ import org.w3c.dom.Text
 class AddSaldoDialogFragment : DialogFragment() {
 
     //TODO: Ver por que no muestra en el EditText
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        if(Preferencias.existeMonedero(this.context!!,Preferencias.getMonedaBase(this.context!!)!!)){
-            dialog.findViewById<EditText>(R.id.etSaldoActual).setText(Preferencias.monedero(this.context!!, Preferencias.getMonedaBase(this.context!!)!!).toString())
-        }
-    }
-
 
    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
         val builder = AlertDialog.Builder(this.context!!)
         var inflater: LayoutInflater = activity!!.layoutInflater
 
+        var vista = inflater.inflate(R.layout.dialog_saldoactual,null)
+        vista.findViewById<EditText>(R.id.etSaldoActual).setText(Preferencias.monedero(this.context!!, Preferencias.getMonedaBase(this.context!!)!!).toString())
         builder .setTitle("Ingrese su saldo actual")
-                .setView(inflater.inflate(R.layout.dialog_saldoactual,null))
+                .setView(vista)
                 .setPositiveButton("Aceptar") { _ , _ ->
                     var valor = dialog.findViewById<EditText>(R.id.etSaldoActual).text.toString()
                     if (!valor.isNullOrEmpty()) {
